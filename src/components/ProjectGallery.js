f9ed7019b9a3f5c3a330105afaf6827cc7a52fb8
Mattie Barton <maitlandbarton@gmail.com>
2023-05-11 14:42:52 +0200

    center project gallery images

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -6,9 +6,9 @@ const ProjectGallery = ({ id }) => {
   const index = id - 1;
 
   return (
-    <div className="grid-rows-2 my-auto">
-    <img className="rounded-xl sm:max-w-screen md:max-w-xl mx-5 md:mx-auto lg:mr-5 my-6 shadow-lg shadow-customBlue" src={oneProject[index].images[0].img}></img>
-    <img className="rounded-xl sm:max-w-screen md:max-w-xl mx-5 md:mx-auto lg:mr-5 my-6 shadow-lg shadow-customBlue" src={oneProject[index].images[1].img}></img>
+    <div className="grid-rows-2 my-auto sm:max-w-screen">
+    <img className="rounded-xl md:max-w-lg mx-5 md:mx-auto lg:mr-5 my-6 shadow-lg shadow-customBlue" src={oneProject[index].images[0].img}></img>
+    <img className="rounded-xl md:max-w-lg mx-5 md:mx-auto lg:mr-5 my-6 shadow-lg shadow-customBlue" src={oneProject[index].images[1].img}></img>
     </div>
   )
 }
