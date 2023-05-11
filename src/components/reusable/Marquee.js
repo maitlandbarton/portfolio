2636feb887aa484b794e48f585a4ac4608f16014
Mattie Barton <maitlandbarton@gmail.com>
2023-05-11 15:22:54 +0200

    responsive design for medium screens and marquee

--- src/components/reusable/Marquee.js
+++ src/components/reusable/Marquee.js
@@ -2,19 +2,19 @@ const Marquee = ({ text1, text2}) => {
     return (
         <div class="relative flex overflow-x-hidden">
         <div class={`py-12 animate-marquee whitespace-nowrap`}>
-          <span class="mx-4 text-6xl">{text1} •</span>
-          <span class="mx-4 text-6xl">{text2} •</span>
-          <span class="mx-4 text-6xl">{text1} •</span>
-          <span class="mx-4 text-6xl">{text2} •</span>
-          <span class="mx-4 text-6xl">{text1} •</span>
+          <span class="mx-4 text-4xl md:text-5xl lg:text-6xl">{text1} •</span>
+          <span class="mx-4 text-4xl md:text-5xl lg:text-6xl">{text2} •</span>
+          <span class="mx-4 text-4xl md:text-5xl lg:text-6xl">{text1} •</span>
+          <span class="mx-4 text-4xl md:text-5xl lg:text-6xl">{text2} •</span>
+          <span class="mx-4 text-4xl md:text-5xl lg:text-6xl">{text1} •</span>
         </div>
 
         <div class={`absolute top-0 py-12 animate-marquee2 whitespace-nowrap`}>
-          <span class="mx-4  text-6xl">{text2} •</span>
-          <span class="mx-4  text-6xl">{text1} •</span>
-          <span class="mx-4  text-6xl">{text2} •</span>
-          <span class="mx-4  text-6xl">{text1} •</span>
-          <span class="mx-4  text-6xl">{text2} •</span>
+          <span class="mx-4  text-4xl md:text-5xl lg:text-6xl">{text2} •</span>
+          <span class="mx-4  text-4xl md:text-5xl lg:text-6xl">{text1} •</span>
+          <span class="mx-4  text-4xl md:text-5xl lg:text-6xl">{text2} •</span>
+          <span class="mx-4  text-4xl md:text-5xl lg:text-6xl">{text1} •</span>
+          <span class="mx-4  text-4xl md:text-5xl lg:text-6xl">{text2} •</span>
         </div>
       </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,27 +12,27 @@ function Home() {
     <div className="lg:h-screen">
       <div className="relative flex overflow-x-hidden">
         <div className="py-12 animate-marquee whitespace-nowrap ">
-          <span className="mx-4 text-6xl">responsive •</span>
-          <span className="mx-4 text-6xl">scalable •</span>
-          <span className="mx-4 text-6xl">responsive •</span>
-          <span className="mx-4 text-6xl">scalable •</span>
-          <span className="ml-4 mr-7 text-6xl">responsive •</span>
+          <span className="mx-4 text-4xl md:text-5xl lg:text-6xl">responsive •</span>
+          <span className="mx-4 text-4xl md:text-5xl lg:text-6xl">scalable •</span>
+          <span className="mx-4 text-4xl md:text-5xl lg:text-6xl">responsive •</span>
+          <span className="mx-4 text-4xl md:text-5xl lg:text-6xl">scalable •</span>
+          <span className="ml-4 mr-7 text-4xl md:text-5xl lg:text-6xl">responsive •</span>
         </div>
 
         <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-          <span className="mr-4 ml-8  text-6xl">scalable •</span>
-          <span className="mx-4  text-6xl">responsive •</span>
-          <span className="mx-4  text-6xl">scalable •</span>
-          <span className="mx-4  text-6xl">responsive •</span>
-          <span className="mx-4  text-6xl">scalable •</span>
+          <span className="mr-4 ml-8  text-4xl md:text-5xl lg:text-6xl">scalable •</span>
+          <span className="mx-4  text-4xl md:text-5xl lg:text-6xl">responsive •</span>
+          <span className="mx-4  text-4xl md:text-5xl lg:text-6xl">scalable •</span>
+          <span className="mx-4  text-4xl md:text-5xl lg:text-6xl">responsive •</span>
+          <span className="mx-4  text-4xl md:text-5xl lg:text-6xl">scalable •</span>
         </div>
       </div>
 
       <div className="flex flex-col lg:flex-row justify-around md:mt-14">
         <div className="lg:w-1/2 flex flex-col px-10 lg:pl-28 pt-5 lg:pt-10">
-          <h1 className="text-4xl md:text-5xl font-semibold mb-3">Hi, I'm Mattie 👋🏻</h1>
-          <h2 className="text-3xl">A Fullstack Web Developer</h2>
-          <h2 className="text-3xl">& Design Enthusiast</h2>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-3">Hi, I'm Mattie 👋🏻</h1>
+          <h2 className="text-2xl md:text-3xl">A Fullstack Web Developer</h2>
+          <h2 className="text-2xl md:text-3xl">& Design Enthusiast</h2>
           <br />
           <p className="text-xl font-thin">
             I have a passion for developing high-quality web apps with a
